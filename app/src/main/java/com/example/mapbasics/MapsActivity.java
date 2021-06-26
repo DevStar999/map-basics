0abcafdcf568633e61789432a0bb66e126d96532
DevStar999 <wolverinerage99@gmail.com>
2021-06-27 00:49:58 +0530

    Using a different color icon for marked location on the map

--- app/src/main/java/com/example/mapbasics/MapsActivity.java
+++ app/src/main/java/com/example/mapbasics/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -41,7 +42,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney)
-                .title("This is a custom marker made by, Vinay Nagalgaonkar"));
+                .title("This is a custom marker made by, Vinay Nagalgaonkar")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+                /* Using a different color icon for marked location on the map as above */
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         /* Hands-on for showing some other location on the map */
