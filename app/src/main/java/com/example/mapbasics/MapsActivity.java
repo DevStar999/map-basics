749ecc749f011645a6320e9a89e4c8e801d3c17c
DevStar999 <wolverinerage99@gmail.com>
2021-06-27 00:03:16 +0530

    Played around with the by-default marker in the java file

--- app/src/main/java/com/example/mapbasics/MapsActivity.java
+++ app/src/main/java/com/example/mapbasics/MapsActivity.java
@@ -40,7 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney)
+                .title("This is a custom marker made by, Vinay Nagalgaonkar"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
